79589698d0932c42150e71a07745d90a9be7e82e
Sam Kleinman <samk@10gen.com>
2013-06-07 15:48:20 -0400

    bootstrap: adding support for cleaning up and restting buildsystem

--- bin/bootstrap_helper.py
+++ bin/bootstrap_helper.py
@@ -17,6 +17,21 @@ def init_fabric(buildsystem, conf_file):
     symlink(name=os.path.join(buildsystem, 'fabfile', 'docs_meta.py'),
             target=os.path.join('../', buildsystem, 'bin', 'docs_meta.py'))
 
+def clean_buildsystem(buildsystem, output_dir):
+    if os.path.islink('fabfile'):
+        os.remove('fabfile')
+        print('[bootstrap-clean]: removed fabfile symlink')
+            
+    import glob
+    for i in glob.glob(os.path.join(output_dir, 'makefile.*')):
+        os.remove(i)
+        print('[bootstrap-clean]: cleaned %s' % i)
+
+    if os.path.exists(buildsystem):
+        import shutil
+        shutil.rmtree(buildsystem)
+        print('[bootstrap-clean]: purged %s' % buildsystem)
+
 def bootstrap():
     """
     The bootstrap file calls this function. Use this as a site for future
