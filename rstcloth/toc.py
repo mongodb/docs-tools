4749f24ca2af0a4542496abf22538fd654a68b81
schmalliso <allison.moore@10gen.com>
2013-07-29 10:47:37 -0400

    build: fixing table and toctree generator
    Signed-off-by: Sam Kleinman <samk@10gen.com>

--- rstcloth/toc.py
+++ rstcloth/toc.py
@@ -58,8 +58,11 @@ class CustomTocTree(object):
     def finalize(self):
         if not self.final:
             for ref in self.spec:
-                if self.table is not None:
-                    self.table.add_row([ ref['name'], ref['description'] ])
+                if self.table is not None: 
+                    if 'name' in ref: 
+                        self.table.add_row([ ref['name'], ref['description'] ])
+                    else:
+                        self.table = None
                 if self.contents is not None:
                     self.contents.content(ref['file'], 6, block='toc')
                 if self.dfn is not None:
@@ -76,6 +79,7 @@ class CustomTocTree(object):
                     
                     self.dfn.definition(link, ref['description'], indent=idnt, bold=False, wrap=False)
                     self.dfn.newline()
+                    
 
 class AggregatedTocTree(CustomTocTree):
     def __init__(self, filename):
@@ -112,44 +116,3 @@ class AggregatedTocTree(CustomTocTree):
             if obj['file'] in filter_docs:
                 obj['level'] = filter_specs[obj['file']] 
                 self.spec.append(obj)
-
-def user_input():
-    parser = argparse.ArgumentParser('table of contents generator.')
-
-    parser.add_argument('filename', nargs='?',
-                        help='the input data file.')
-    parser.add_argument('--table', '-t', action='store', default=False,
-                        help='output filename for table.')
-    parser.add_argument('--contents', '-c', action='store', default=False,
-                        help='output filename for toctree.')
-    parser.add_argument('--dfn', '-d', action='store', default=False,
-                        help='output filename for definition list.')
-    parser.add_argument('--sort', '-s', action='store_true', default=False,
-                        help='sort items in toc output filename for lists.')
-
-    return parser.parse_args()
-
-def main():
-    ui = user_input()
-
-    toc = CustomTocTree(ui.filename)
-
-    if ui.dfn:
-        toc.build_dfn()
-    if ui.contents:
-        toc.build_contents()
-    if ui.table:
-        toc.build_table()
-
-    toc.finalize()
-
-    if ui.dfn:
-        toc.dfn.write(ui.dfn)
-    if ui.contents:
-        toc.contents.write(ui.contents)
-    if ui.table:
-        t = tb.TableBuilder(tb.RstTable(toc.table))
-        t.write(ui.table)
-
-if __name__ == '__main__':
-    main()
