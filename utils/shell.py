54fffa9007a274cf6fded0c1c01230aa53f4e4ab
Sam Kleinman <samk@10gen.com>
2014-01-15 10:21:14 -0500

    shell: adding fabric-like independent subprocess wrapper

--- utils/shell.py
+++ utils/shell.py
@@ -1,6 +1,13 @@
-import subprocess
 import os
 import re
+import subprocess
+
+try:
+    from structures import AttributeDict
+except ImportError:
+    # support for bootstrapping
+    from utils.structures import AttributeDict
+
 
 def shell_value(args, path=None):
     if path is None:
@@ -14,3 +21,47 @@ def shell_value(args, path=None):
     r = p.communicate()
 
     return str(r[0].decode().rstrip())
+
+class CommandError(Exception): pass
+
+def command(command, capture=False, ignore=False):
+    dev_null = None
+    if capture:
+        out_stream = subprocess.PIPE
+        err_stream = subprocess.PIPE
+    else:
+        dev_null = open(os.devnull, 'w+')
+        # Non-captured, hidden streams are discarded.
+        out_stream = dev_null
+        err_stream = dev_null
+    try:
+        p = subprocess.Popen(command, shell=True, stdout=out_stream,
+                             stderr=err_stream)
+
+        (stdout, stderr) = p.communicate()
+    finally:
+        if dev_null is not None:
+            dev_null.close()
+
+    out = {
+        'cmd': command,
+        'err': stderr.strip() if stdout else "",
+        'out': stdout.strip() if stdout else "",
+        'return_code': p.returncode,
+        'succeeded': True if p.returncode == 0 else False,
+        'failed': False if p.returncode == 0 else True
+    }
+
+    out = AttributeDict(out)
+
+    if ignore is True:
+        return out
+    elif out.succeeded is True:
+        if capture is True:
+            return out
+        else:
+            return None
+    else:
+        print('[ERROR]: ' + out.cmd)
+        print('[ERROR]: returned ' + out.return_code)
+        raise CommandError
