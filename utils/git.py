7af8ad2cda90d0387487943c8ef30141867e3217
Sam Kleinman <samk@10gen.com>
2014-01-15 10:21:40 -0500

    git: adding cross branch file resolution

--- utils/git.py
+++ utils/git.py
@@ -1,10 +1,10 @@
 import os
 
 try:
-    from shell import shell_value
+    from shell import shell_value, command
 except ImportError:
     # so bootstrapping works
-    from utils.shell import shell_value
+    from utils.shell import shell_value, command
 
 def get_commit(path=None):
     if path is None:
@@ -17,3 +17,13 @@ def get_branch(path=None):
         path = os.getcwd()
 
     return shell_value('git symbolic-ref HEAD', path).split('/')[2]
+
+def checkout_file(path, branch='master'):
+    return command(command='git checkout {0} -- {1}'.format(branch, path),
+                   capture=False, ignore=False)
+
+def get_file_from_branch(path, branch='master'):
+    cmd = 'git show {branch}:{path}'.format(branch=branch, path=path)
+    out = command(command=cmd, capture=True, ignore=False)
+
+    return out.out
