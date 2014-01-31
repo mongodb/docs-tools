05dec9f38d63533807afb44dc6ef16133272962f
Sam Kleinman <samk@10gen.com>
2014-01-30 20:27:15 -0500

    sphinx: support ecosystem builds

--- utils/sphinx/prepare.py
+++ utils/sphinx/prepare.py
@@ -24,7 +24,7 @@ from utils.contentlib.source import transfer_source
 from utils.sphinx.dependencies import refresh_dependencies
 
 def build_prereq_jobs(conf):
-    if conf.project.name != "mms":
+    if conf.project.name not in [ "mms", "ecosystem" ]:
 
         jobs = [
             {
