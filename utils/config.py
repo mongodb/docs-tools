587fba68c37f66d12b7a8f8a41de162530c38095
Sam Kleinman <samk@10gen.com>
2014-02-10 18:16:15 -0500

    minor+config: inverting logic

--- utils/config.py
+++ utils/config.py
@@ -105,7 +105,7 @@ def render_versions(conf=None):
             vconf_data = get_file_from_branch(version_config_file, 'master')
         except CommandError:
             remotes = command('git remote', capture=True).out.split('\n')
-            if 'config-upstream' in remotes:
+            if 'config-upstream' not in remotes:
                 command('git remote add config-upstream git://github.com/{0}.git'.format(conf.git.remote.upstream))
             else:
                 command('git fetch config-upstream')
