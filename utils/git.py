4ca22123e66adcda06216d39275941fc5ac870f8
tycho garen <garen@tychoish.com>
2014-02-22 18:23:44 -0500

    shell+git: modernizing and standardizing access

--- utils/git.py
+++ utils/git.py
@@ -1,21 +1,15 @@
 import os
 
 try:
-    from utils.shell import shell_value, command
+    from utils.shell import command
 except ImportError:
-    from shell import shell_value, command
+    from shell import command
 
 def get_commit(path=None):
-    if path is None:
-        path = os.getcwd()
-
-    return shell_value('git rev-parse --verify HEAD', path)
+    return command('git rev-parse --verify HEAD', capture=True).out
 
 def get_branch(path=None):
-    if path is None:
-        path = os.getcwd()
-
-    return shell_value('git symbolic-ref HEAD', path).split('/')[2]
+    return command('git symbolic-ref HEAD', capture=True).out.split('/')[2]
 
 def checkout_file(path, branch='master'):
     return command(command='git checkout {0} -- {1}'.format(branch, path),
@@ -23,6 +17,5 @@ def checkout_file(path, branch='master'):
 
 def get_file_from_branch(path, branch='master'):
     cmd = 'git show {branch}:{path}'.format(branch=branch, path=path)
-    out = command(command=cmd, capture=True, ignore=False)
 
-    return out.out
+    return command(command=cmd, capture=True, ignore=False).out
--- utils/shell.py
+++ utils/shell.py
@@ -1,5 +1,4 @@
 import os
-import re
 import subprocess
 from tempfile import NamedTemporaryFile
 
@@ -8,20 +7,6 @@ try:
 except ImportError:
     # support for bootstrapping
     from utils.structures import AttributeDict
-
-
-def shell_value(args, path=None):
-    if path is None:
-        path = os.getcwd()
-
-    if isinstance(args , str):
-        r = re.compile("\s+")
-        args = r.split(args)
-
-    p = subprocess.Popen(args, cwd=path, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    r = p.communicate()
-
-    return str(r[0].decode().rstrip())
 
 class CommandError(Exception): pass
 
@@ -49,27 +34,21 @@ def command(command, capture=False, ignore=False):
         stderr = ""
     else:
         with open(tmp_out.name, 'r') as f:
-            stdout = ''.join(f.readlines())
+            stdout = ''.join(f.readlines()).strip()
         with open(tmp_err.name, 'r') as f:
-            stderr = ''.join(f.readlines())
+            stderr = ''.join(f.readlines()).strip()
 
-    out = {
+    out = AttributeDict({
         'cmd': command,
         'err': stderr,
         'out': stdout,
         'return_code': p.returncode,
         'succeeded': True if p.returncode == 0 else False,
-        'failed': False if p.returncode == 0 else True
-    }
+        'failed': False if p.returncode == 0 else True,
+        'captured': capture,
+    })
 
-    out = AttributeDict(out)
-
-    if ignore is True:
+    if out.succeeded is True or ignore is True:
         return out
-    elif out.succeeded is True:
-        if capture is True:
-            return out
-        else:
-            return None
     else:
         raise CommandError('[ERROR]: "{0}" returned {1}'.format(out.cmd, out.return_code))
