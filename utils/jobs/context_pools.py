967d987a9313e61e71239e7c09fdff2a65470e23
tycho garen <garen@tychoish.com>
2014-02-22 20:35:52 -0500

    jobs: making context pool size configurable

--- utils/jobs/context_pools.py
+++ utils/jobs/context_pools.py
@@ -1,10 +1,20 @@
 import multiprocessing.dummy
+from multiprocessing import cpu_count
 
-from utils.jobs.pool import NestedPool
-from utils.jobs.pool import NestedPool
-from utils.jobs.runners import async_job_loop, process_async_results
+try:
+    from utils.jobs.pool import NestedPool
+    from utils.jobs.runners import async_job_loop, process_async_results
+except ImportError:
+    from pool import NestedPool
+    from runners import async_job_loop, process_async_results
 
 class WorkerPool(object):
+    def __init__(self, size=None):
+        if size is None:
+            self.size = cpu_count()
+        else:
+            self.size = size
+
     def __exit__(self, *args):
         self.p.close()
         self.p.join()
@@ -16,14 +26,14 @@ class WorkerPool(object):
 
 class ThreadPool(WorkerPool):
     def __enter__(self):
-        self.p = multiprocessing.dummy.Pool()
+        self.p = multiprocessing.dummy.Pool(self.size)
         self.p.runner = self.runner
 
         return self.p
 
 class ProcessPool(WorkerPool):
     def __enter__(self):
-        self.p = NestedPool()
+        self.p = NestedPool(self.size)
         self.p.runner = self.runner
 
         return self.p
