e10c2f7a9b58496569ce1a0e6e068267ec608357
Sam Kleinman <samk@10gen.com>
2014-05-20 16:32:56 -0400

    giza: additional modeling

--- giza/giza.py
+++ giza/giza.py
@@ -8,23 +8,30 @@ logging.basicConfig(level=logging.INFO) # set basic default log level
 import argh
 
 from app import BuildApp
-from configuration import Configuration
+from configuration import Configuration, RuntimeStateConfiguration
 
 @argh.arg('--confp')
 def test(arg):
-    c = Configuration(arg.confp)
+    c = Configuration()
+    c.ingest(arg.confp)
+    r = RuntimeStateConfiguration()
+    c.runstate = r
 
-    print(c.git.commit)
-    print(c.git.dict())
-    print(c.git.branches.repo.current_branch())
+    print(hasattr(c, 'project'))
+    dynamics = [ c.git.commit, c.paths.public, c.git.branches.current, c.git.branches.manual,
+                 c.git.branches.published, c.paths.branch_output, c.paths.buildarchive,
+                 c.paths.branch_source, c.paths.branch_staging, c.version.published,
+                 c.version.stable, c.version.upcoming,
+                 c.paths.global_config ]
 
-    print(c.git.branches.current)
-    print('--- ' + "dir of rendered object >>>")
-    print(dir(c))
+    print(dynamics)
+
+
+    # print('--- ' + "dir of rendered object >>>")
+    # print(dir(c))
     print('--- ' + "str of object >>>")
     print(json.dumps(c.dict(), indent=3))
     print('---  >>>')
-
 
 def main():
     parser = argh.ArghParser()
