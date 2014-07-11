eb43417aab6a37c606a0de558039adc0fcdece1b
Sam Kleinman <samk@10gen.com>
2014-07-11 17:20:44 -0400

    minor: fixing log processing log.

--- giza/giza/content/sphinx.py
+++ giza/giza/content/sphinx.py
@@ -105,10 +105,14 @@ def output_sphinx_stream(out, conf):
 
         l = path_normalization(l, full_path, conf)
 
-        if l.startswith('InputError: [Errno 2] No such file or directory') and idx != 0:
-            l = path_normalization(l.split(' ')[-1].strip()[1:-2], full_path, conf)
-            printable[idx-1] += ' ' + l
-            l = None
+        if l.startswith('InputError: [Errno 2] No such file or directory'):
+            try:
+                l = path_normalization(l.split(' ')[-1].strip()[1:-2], full_path, conf)
+                printable[idx-1] += ' ' + l
+                l = None
+            except IndexError:
+                logger.error("error processing log: {0}".format(l))
+                continue
         elif l.startswith('source/includes/generated/overview.rst'):
             continue
         elif l.startswith('source/meta/includes.txt'):
