f4fea8f88bd89afc7140db381a128d569fc8c507
Sam Kleinman <samk@10gen.com>
2015-02-02 13:24:35 -0500

    giza: fix syntax

--- giza/giza/content/extract/models.py
+++ giza/giza/content/extract/models.py
@@ -14,6 +14,7 @@
 
 import logging
 import os.path
+import sys
 
 logger = logging.getLogger('giza.content.extract.models')
 
@@ -22,7 +23,6 @@ from giza.content.steps.models import HeadingMixin
 
 if sys.version_info >= (3, 0):
     basestring = str
-
 
 class ExtractData(HeadingMixin, InheritableContentBase):
     _default_level = 2
