6d7b505e6cf095700e25caf010db12702d45e6cc
Andrew Aldridge <i80and@foxquill.com>
2016-01-29 09:23:06 -0500

    Implement FastLoad navbar mode

--- themes/mongodb/static/navbar.js
+++ themes/mongodb/static/navbar.js
@@ -1,4 +1,6 @@
 $(function() {
+    'use strict';
+
     var docsExcludedNav = window.docsExcludedNav;
 
     /* Checks a whitelist for non-leaf nodes that should trigger a full page reload */
@@ -24,69 +26,203 @@ $(function() {
         return !$node.siblings('ul:not(.simple)').length;
     }
 
-    var $current = $('.sidebar a.current');
-    if (isLeafNode($current) || requiresPageload($current) || isCurrentNode($current)) {
-        $current.parent('li').addClass('selected-item');
+    function updateSidebar() {
+        var $current = $('.sidebar a.current');
+        if (isLeafNode($current) || requiresPageload($current) || isCurrentNode($current)) {
+            $current.parent('li').addClass('selected-item');
+        }
+        $current.parents('ul').each(function(index, el) {
+            $(el).css('display', 'block');
+        });
+
+        $('.sphinxsidebarwrapper > ul li:not(.current) > ul:not(.current)').hide();
+        $('.sphinxsidebarwrapper').show();
+
+        /*
+         * This event handler defines the left-column navigation's behavior
+         * The logic conforms to how sphinx generates the markup
+         * The rules are:
+         *  $('a.current') is the title of the content that is currently rendered (this should never be changed client-side)
+         *  $('ul.current, li.current') are the set of lists that are currently expanded
+         *  $('li.selected-item') is the currently highlighted item
+         */
+        $('.sphinxsidebarwrapper .toctree-l1').on('click', 'a', function(e) {
+            var $target = $(e.currentTarget);
+            if (isLeafNode($target)) {
+                return; // Do a full page reload on leaf nodes
+            }
+
+            // Release notes has special behavior to click through
+            if (!$target.parent().hasClass('selected-item') && requiresPageload($target)) {
+                return;
+            }
+
+            e.preventDefault();
+
+            if ($target.parent().hasClass('current')) {
+                // collapse target node
+                $target.parent().removeClass('current selected-item');
+                $target.siblings('ul').slideUp();
+            } else {
+
+                $current.parent().removeClass('selected-item');
+                // roll up all navigation up to the common ancestor
+                $current.parents().add($current.siblings('ul')).each(function(index, el) {
+                    var $el = $(el);
+                    if ($el.has(e.currentTarget).length) {
+                        return;
+                    }
+
+                    if ($el.is('ul')) {
+                        $el.removeClass('current').slideUp();
+                    } else {
+                        $el.removeClass('current');
+                    }
+                });
+                // add css classes for the new 'current' nav item
+                $target.parent().addClass('current');
+                $target.siblings('ul').slideDown(function() {
+                    if (isLeafNode($target) || requiresPageload($target) || isCurrentNode($current)) {
+                        $target.parent('li').addClass('selected-item');
+                    }
+                });
+                // set new $current
+                $current = $target;
+            }
+        });
+
+        /* Add expand icons to indicate what's expandable and what's a link. This
+           is necessary when (1) we're at a leaf node, or (2) the menu triggers
+           a page load. */
+        $('.sphinxsidebarwrapper > ul ul a.reference').prepend(function(index) {
+            var expandElement = $('<span class="expand-icon"></span>');
+            var self = $(this);
+
+            if(!isLeafNode(self)) {
+                expandElement.addClass('fa fa-plus');
+            }
+
+            return expandElement;
+        });
     }
-    $current.parents('ul').each(function(index, el) {
-        $(el).css('display', 'block');
-    });
 
-    $('.sphinxsidebarwrapper > ul li:not(.current) > ul:not(.current)').hide();
-    $('.sphinxsidebarwrapper').show();
-
-    /*
-     * This event handler defines the left-column navigation's behavior
-     * The logic conforms to how sphinx generates the markup
-     * The rules are:
-     *  $('a.current') is the title of the content that is currently rendered (this should never be changed client-side)
-     *  $('ul.current, li.current') are the set of lists that are currently expanded
-     *  $('li.selected-item') is the currently highlighted item
-     */
-    $('.sphinxsidebarwrapper .toctree-l1').on('click', 'a', function(e) {
-        var $target = $(e.currentTarget);
-        if (isLeafNode($target)) {
-            return; // Do a full page reload on leaf nodes
+    // If the browser is sufficiently modern, make navbar links load only
+    // content pieces to avoid a full page load.
+    function setupFastLoad() {
+        if (window.history === undefined ||
+            document.querySelectorAll === undefined ||
+            document.body.classList === undefined) {
+            return false;
         }
 
-        // Release notes has special behavior to click through
-        if (!$target.parent().hasClass('selected-item') && requiresPageload($target)) {
-            return;
+        var navRootElement = document.querySelector('.sphinxsidebarwrapper');
+        var bodyElement = document.querySelector('.body');
+        var curLoading = {};
+
+        // Stop loading the currently-in-progress page.
+        var abortLoading = function() {
+            if (curLoading.timeoutID !== undefined) {
+                window.clearTimeout(curLoading.timeoutID);
+            }
+
+            if (curLoading.ajax !== undefined) {
+                curLoading.ajax.abort();
+            }
+
+            curLoading = {};
+        };
+
+        // Load the specified URL.
+        var loadPage = function(href, createHistory) {
+            if (href === undefined) {
+                console.error('Going to undefined path');
+            }
+
+            abortLoading();
+            bodyElement.classList.add('loading');
+
+            // If something goes wrong while loading, we don't want to leave
+            // people without a paddle. If we can't load after a long period of
+            // time, bring back the original content.
+            curLoading.timeoutID = window.setTimeout(function() {
+                bodyElement.classList.remove('loading');
+                curLoading.timeoutID = -1;
+            }, 10000);
+
+            var startTime = new Date();
+            curLoading.ajax = $.ajax({ url: href, dataType: 'html', success: function(pageText) {
+                var enlapsedMs = (new Date()) - startTime;
+                bodyElement.classList.remove('loading');
+
+                var page = document.createElement('html');
+                page.innerHTML = pageText;
+                var title = page.querySelector('title').textContent;
+                var newBody = page.querySelector('.body');
+                var newNav = page.querySelector('.sphinxsidebarwrapper');
+
+                // Fade in ONLY if we had enough time to fade out at least some.
+                if (enlapsedMs > (250 / 4)) {
+                    newBody.classList.add('loading');
+                }
+
+                // Replace the DOM elements
+                bodyElement.parentElement.replaceChild(newBody, bodyElement);
+                bodyElement = newBody;
+                navRootElement.parentElement.replaceChild(newNav, navRootElement);
+                navRootElement = newNav;
+                document.title = title;
+
+                if (createHistory) {
+                    window.history.pushState({ href: href }, title, href);
+                }
+
+                // Update the sidebar
+                updateSidebar();
+                setupFastLoad();
+
+                // Prime the new DOM so that we can set up our fade-in
+                // animation and scroll the new contents to the top.
+                window.setTimeout(function() {
+                    bodyElement.classList.remove('loading');
+                    window.scroll(0, 0);
+                }, 1);
+            }, error: function(ev) {
+                // Some browsers consider any file://-type request to be cross-origin.
+                // In this case, fall back to old-style behavior.
+                if (ev.status === 0 && ev.statusText === 'error') {
+                    window.location = href;
+                }
+
+                console.error('Failed to load ' + href);
+            }, complete: function() {
+                abortLoading();
+            } });
+        };
+
+        var nodes = document.querySelectorAll('.sphinxsidebarwrapper > ul a.reference');
+        var handleClickFunction = function(ev) {
+            // Ignore anything but vanilla click events, so that people can
+            // still use special browser behaviors like open in new tab.
+            if (!(ev.button !== 0 || ev.shiftKey || ev.altKey || ev.metaKey || ev.ctrlKey)) {
+                ev.preventDefault();
+                loadPage(ev.currentTarget.href, true);
+            }
+        };
+        for (var i = 0; i < nodes.length; i += 1) {
+            var node = nodes[i];
+            if (!isLeafNode($(node)) && !requiresPageload($(node))) { continue; }
+
+            node.addEventListener('click', handleClickFunction);
         }
 
-        e.preventDefault();
+        window.onpopstate = function(ev) {
+            if (ev.state === null) { return; }
+            loadPage(ev.state.href, false);
+        };
 
-        if ($target.parent().hasClass('current')) {
-            // collapse target node
-            $target.parent().removeClass('current selected-item');
-            $target.siblings('ul').slideUp();
-        } else {
 
-            $current.parent().removeClass('selected-item');
-            // roll up all navigation up to the common ancestor
-            $current.parents().add($current.siblings('ul')).each(function(index, el) {
-                var $el = $(el);
-                if ($el.has(e.currentTarget).length) {
-                    return;
-                }
-
-                if ($el.is('ul')) {
-                    $el.removeClass('current').slideUp();
-                } else {
-                    $el.removeClass('current');
-                }
-            });
-            // add css classes for the new 'current' nav item
-            $target.parent().addClass('current');
-            $target.siblings('ul').slideDown(function() {
-                if (isLeafNode($target) || requiresPageload($target) || isCurrentNode($current)) {
-                    $target.parent('li').addClass('selected-item');
-                }
-            });
-            // set new $current
-            $current = $target;
-        }
-    });
+        return true;
+    }
 
     /* Options panel */
     $('.option-header').on('click', function(e) {
@@ -155,17 +291,6 @@ $(function() {
         }
     });
 
-    /* Add expand icons to indicate what's expandable and what's a link. This
-       is necessary when (1) we're at a leaf node, or (2) the menu triggers
-       a page load. */
-    $('.sphinxsidebarwrapper > ul ul a.reference').prepend(function(index) {
-        var expandElement = $('<span class="expand-icon"></span>');
-        var self = $(this)
-
-        if(!isLeafNode(self)) {
-            expandElement.addClass('fa fa-plus');
-        }
-
-        return expandElement;
-    });
+    updateSidebar();
+    setupFastLoad();
 });
