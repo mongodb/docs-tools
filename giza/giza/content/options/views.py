6355792bbee0cd41d8259f9ba321fccfb1a8cd4c
Andrew Aldridge <i80and@foxquill.com>
2017-02-21 16:18:48 -0500

    Revert "giza options: generate 'program' directive for each option"
    This reverts commit e3c23dfa2e96fc61efa05aa8e38dcc9915fedd84.

--- giza/giza/content/options/views.py
+++ giza/giza/content/options/views.py
@@ -57,7 +57,6 @@ def render_options(option, conf):
         option.replacement['role'] = ':{0}:`{1}{2}`'.format(option.directive, prefix, option.name)
 
     option.render()  # jinja template render
-    r.directive('program', option.program)
     r.directive(option.directive, directive_str)
     r.newline()
 
