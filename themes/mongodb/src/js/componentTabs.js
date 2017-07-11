5e652fe1d10221cce2bd6f120dd381d2170dad07
Andrew Aldridge <i80and@foxquill.com>
2017-07-11 16:08:22 -0400

    Refactor tabs with partial BEM structure

--- themes/mongodb/src/js/componentTabs.js
+++ themes/mongodb/src/js/componentTabs.js
@@ -1,111 +1,132 @@
-// Show the appropriate tab content and hide other tab's content
+/**
+ * Show the appropriate tab content and hide other tab's content
+ * @param {string} currentAttrValue The currently selected tab ID.
+ * @returns {void}
+ */
 function showHideTabContent(currentAttrValue) {
-    // Remove the # to find <div> w/ class of the currentAttrValue
-    if (currentAttrValue.charAt(0) === '#') {
-        currentAttrValue = currentAttrValue.substring(1);
-    }
     $('.tab-content').children().
         hide();
-    $(`.tabs .${currentAttrValue}`).show();
-
+    $(`.tabs .tabpanel-${currentAttrValue}`).show();
 }
 
-// Marks the selected tab as active, handles special cases for the dropdown
-function showHideSelectedTab(currentAttrValue) {
-    // Get the <a>, <li> and <ul> of the selected tab
-    const tabLink = $(`a[href="${currentAttrValue}"]`);
-    const tabListItem = tabLink.parent('li');
-    const tabList = tabListItem.parent('ul');
-
-    // Get the dropdown <a> and <li> for active and label management
-    const dropdownLink = $('.nav.nav-tabs.nav-justified .dropdown-toggle');
-    const dropdownListItem = $('.nav.nav-tabs.nav-justified .dropdown');
-
-    // Set the active tab, if it's on the dropdown set it to active and change label
-    if (tabList.hasClass('dropdown-menu')) {
-        // Use first so text doesn't repeat if more than one set of tabs
-        dropdownLink.text(`${tabLink.first().text()}`).append('<span class="caret"></span>');
-        dropdownListItem.
-            addClass('active').
-            siblings().
-            removeClass('active');
-    } else {
-        // Set a non-dropdown tab to active, and change the dropdown label back to "Other"
-        tabListItem.
-            addClass('active').
-            siblings().
-            removeClass('active');
-        dropdownLink.text('Other ').append('<span class="caret"></span>');
+class TabsSingleton {
+    constructor(key) {
+        this.key = key;
+        this.tabStrip = document.querySelector('.tab-strip--singleton');
     }
-}
 
-// Show only the first set of tabs at the top of the page
-function hideTabBars() {
-    const tabBars = $('.nav.nav-tabs.nav-justified');
-    const mainTabBar = tabBars.first();
-    // Remove any additional tab bars
-    tabBars.slice(1).
-        detach();
-    // Position the main tab bar after the page title
-    mainTabBar.
-        detach().
-        insertAfter('h1').
-        first();
+    get languagePref() {
+        return window.localStorage.getItem(this.key);
+    }
+
+    set languagePref(value) {
+        window.localStorage.setItem(this.key, value);
+    }
+
+    /**
+     * Return the first singleton tab ID on the page.
+     * @returns {string} The first singleton tab ID found.
+     */
+    getFirstLanguage() {
+        const tabsElement = this.tabStrip.querySelector('.tab-strip__element[aria-selected=true]');
+        if (!tabsElement) { return null; }
+
+        return tabsElement.getAttribute('data-tabid');
+    }
+
+    setup() {
+        if (!this.tabStrip) { return; }
+
+        this.hideTabBars();
+
+        for (const element of this.tabStrip.querySelectorAll('[data-tabid]')) {
+            element.onclick = (e) => {
+                // Get the tab ID of the clicked tab
+                const currentAttrValue = element.getAttribute('data-tabid');
+
+                // Check to make sure value is not null, i.e., don't do anything on "other"
+                if (currentAttrValue) {
+                    // Save the users preference and re-render
+                    this.languagePref = currentAttrValue;
+                    this.update();
+
+                    e.preventDefault();
+                }
+            };
+        }
+
+        this.update();
+    }
+
+    update() {
+        if (!this.tabStrip) { return; }
+
+        let languagePref = this.languagePref;
+        if (!languagePref) {
+            languagePref = this.getFirstLanguage();
+        } else if (!this.tabStrip.querySelector(`[data-tabid="${languagePref}"]`)) {
+            // Confirm a tab for their languagePref exists at the top of the page
+            languagePref = this.getFirstLanguage();
+        }
+
+        if (!languagePref) { return; }
+
+        // Show the appropriate tab content and mark the tab as active
+        showHideTabContent(languagePref);
+        this.showHideSelectedTab(languagePref);
+    }
+
+    /**
+     * Marks the selected tab as active, handles special cases for the dropdown
+     * @param {string} currentAttrValue The currently selected tab ID.
+     * @returns {void}
+     */
+    showHideSelectedTab(currentAttrValue) {
+        // Get the <a>, <li> and <ul> of the selected tab
+        const tabLink = $(this.tabStrip.querySelector(`[data-tabid="${currentAttrValue}"]`));
+        const tabList = tabLink.parent('ul');
+
+        // Get the dropdown <a> and <li> for active and label management
+        const dropdownLink = $(this.tabStrip.querySelector('.dropdown-toggle'));
+        const dropdownListItem = $(this.tabStrip.querySelector('.dropdown'));
+
+        // Set the active tab, if it's on the dropdown set it to active and change label
+        if (tabList.hasClass('dropdown-menu')) {
+            // Use first so text doesn't repeat if more than one set of tabs
+            dropdownLink.text(`${tabLink.first().text()}`).append('<span class="caret"></span>');
+            dropdownListItem.
+                attr('aria-selected', true).
+                siblings().
+                attr('aria-selected', false);
+        } else {
+            // Set a non-dropdown tab to active, and change the dropdown label back to "Other"
+            tabLink.
+                attr('aria-selected', true).
+                siblings().
+                attr('aria-selected', false);
+            dropdownLink.text('Other ').append('<span class="caret"></span>');
+        }
+    }
+
+    /**
+     * Show only the first set of tabs at the top of the page.
+     * @returns {void}
+     */
+    hideTabBars() {
+        const tabBars = $('.tab-strip--singleton');
+        const mainTabBar = tabBars.first();
+        // Remove any additional tab bars
+        tabBars.slice(1).
+            detach();
+        // Position the main tab bar after the page title
+        mainTabBar.
+            detach().
+            insertAfter('h1').
+            first();
+    }
 }
 
 // Create tab functionality for code examples
 export function setup() {
-    hideTabBars();
-
-    let initialAttrValue = null;
-
-    // Check if the user has a preference stored, if so load it
-    if (localStorage.getItem('languagePref')) {
-        initialAttrValue = localStorage.getItem('languagePref');
-
-        // Confirm a tab for their languagePref exists at the top of the page
-        if ($(`a[href="${initialAttrValue}"]`).length < 1) {
-
-            // If not, get the first tab and set that as the active tab
-            if (document.querySelector('.nav.nav-tabs.nav-justified > li:first-child > a')) {
-                initialAttrValue = document.querySelector('.nav.nav-tabs.nav-justified > li:first-child > a').getAttribute('href');
-            }
-        }
-
-    } else {
-        const tabsElement = document.querySelector('.nav-tabs > .active > [href]');
-        if (!tabsElement) {
-            return;
-        }
-
-        initialAttrValue = tabsElement.getAttribute('href');
-    }
-
-    // Show the appropriate tab content and mark the tab as active
-    showHideTabContent(initialAttrValue);
-    showHideSelectedTab(initialAttrValue);
-
-    const tabLinks = document.querySelectorAll('.nav.nav-tabs.nav-justified a');
-
-    for (let i = 0; i < tabLinks.length; i += 1) {
-        const element = tabLinks[i];
-
-        element.onclick = function(e) {
-            // Get the href of the clicked tab
-            const currentAttrValue = element.getAttribute('href');
-
-            // Check to make sure value is not null, i.e., don't do anything on "other"
-            if (currentAttrValue) {
-                // Save the users preference
-                localStorage.setItem('languagePref', currentAttrValue);
-
-                // Show the appropriate tab content and mark the tab as active
-                showHideTabContent(currentAttrValue);
-                showHideSelectedTab(currentAttrValue);
-                hideTabBars();
-
-                e.preventDefault();
-            }
-        };
-    }
+    (new TabsSingleton('languagePref')).setup();
 }
