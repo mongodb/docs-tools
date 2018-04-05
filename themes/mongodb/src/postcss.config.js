9dd775dd532a6d0cba0a44fac7f11652951fcd7c
Andrew Aldridge <i80and@foxquill.com>
2018-04-05 14:38:43 -0400

    theme: Use postcss to compile CSS

---
+++ themes/mongodb/src/postcss.config.js
@@ -0,0 +1,21 @@
+module.exports = ctx => ({
+  map: false,
+  plugins: {
+    'postcss-import': { root: ctx.file.dirname },
+    'postcss-cssnext': {
+        browsers: "defaults, IE >= 10",
+        warnings: true,
+        preserve: false
+    },
+    'postcss-clean': {
+        level: {
+            2: {
+                mergeSemantically: true,
+                restructureRules: true
+            }
+        },
+        skipRebase: true,
+        inline: 'local,fonts.googleapis.com'
+    }
+  }
+})
