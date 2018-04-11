dcfa85e432dc56932abde82b435b0651a5e99cda
Nick Larew <nick.larew@10gen.com>
2018-04-11 14:53:11 -0400

    (DOCSP-2198): Landing Page Refresh

--- themes/mongodb/src/postcss.config.js
+++ themes/mongodb/src/postcss.config.js
@@ -2,6 +2,7 @@ module.exports = ctx => ({
   map: false,
   plugins: {
     'postcss-import': { root: ctx.file.dirname },
+    'postcss-nested': { },
     'postcss-cssnext': {
         browsers: "defaults, IE >= 10",
         warnings: true,
