07dd8a662c0259aaeb891bb86e531f1765c1c744
Nick Larew <nick.larew@10gen.com>
2018-04-27 18:25:40 -0400

    codepen iframe

--- sphinxext/codepen.py
+++ sphinxext/codepen.py
@@ -1,21 +1,29 @@
 from docutils import statemachine
 from docutils.parsers.rst import Directive
+from docutils.parsers.rst import directives
 import fett
 
 CODEPEN_TEMPLATE = fett.Template('''
 .. raw:: html
 
-   <p class="codepen"
-      data-pen-title="{{ slug escape }}"
-      data-slug-hash="{{ slug escape }}"
-      data-height="600"
-      data-theme-id="32535"
-      data-default-tab="js,result"
-      data-user="mongodb-docs"
-      data-embed-version="2"
-      data-editable="true">
-     <a href="https://codepen.io/mongodb-docs/pen/{{ slug escape }}">See this example on codepen: {{ slug escape }}</a>
-   </p>
+   <iframe
+     height={{ height }}
+     scrolling='no'
+     title={{ title escape }}
+     src='//codepen.io/mongodb-docs/embed/{{ slug escape }}/?height={{ height }}&theme-id=32535&default-tab=js,result&embed-version=2&editable=true'
+     frameborder='no'
+     allowtransparency='true'
+     allowfullscreen='true'
+     style='width: 100%;'
+   >   
+     See the Pen
+     <a href='https://codepen.io/mongodb-docs/pen/{{ slug escape }}/'>
+       {{ title escape }}
+     </a>
+     by Shannon Bradshaw (<a href='https://codepen.io/mongodb-docs'>@mongodb-docs</a>)
+     on <a href='https://codepen.io'>CodePen</a>.
+   </iframe>
+
 ''')
 
 
@@ -24,12 +32,27 @@ class CodepenDirective(Directive):
     required_arguments = 1
     optional_arguments = 0
     final_argument_whitespace = True
+    option_spec = {
+        'height': directives.nonnegative_int,
+        'title': directives.unchanged,
+    }
 
     def run(self):
-        data = {'slug': self.arguments[0]}
+        options = self.options
+        data = {
+            'slug': self.arguments[0],
+            'height': 600,
+            'title': "MongoDB Stitch Example"
+        }
+        if options.get('height'):
+            data['height'] = options['height']
+        if options.get('title'):
+            data['title'] = options['title']
+
         rendered = CODEPEN_TEMPLATE.render(data)
         rendered_lines = statemachine.string2lines(
-            rendered, 4, convert_whitespace=1)
+            rendered, 4, convert_whitespace=1
+        )
         self.state_machine.insert_input(rendered_lines, '')
 
         return []
