0838ea217e91c92652ecd74b6664650b536ba0bf
Nick Larew <nick.larew@10gen.com>
2018-04-30 16:54:59 -0400

    landing card image handling

--- themes/mongodb/src/js/componentCodepen.js
+++
@@ -1,9 +0,0 @@
-export function setup() {
-    const codepenBlocks = document.getElementsByClassName('codepen');
-    if (codepenBlocks.length) {
-        const codepenScript = document.createElement('script');
-        codepenScript.src = 'https://production-assets.codepen.io/assets/embed/ei.js';
-        codepenScript.async = true;
-        document.body.appendChild(codepenScript);
-    }
-}
