8cc4a1a5a9b15506f3fe1c4844253fe6deb0ed83
jeff-allen-mongo <jeffrey.allen@10gen.com>
2018-05-10 15:50:24 -0400

    Adding logintoatlas function

--- tools/screenshot-tool/screenshots.js
+++ tools/screenshot-tool/screenshots.js
@@ -1,21 +1,43 @@
 'use strict';
 
-var fs = require('fs-extra');
-var Nightmare = require('nightmare');
-var screenshotSelector = require('nightmare-screenshot-selector');
-var path = require('path');
-var PropertiesReader = require('properties-reader');
-//var properties = PropertiesReader(__dirname + '/.properties.ini');
-var util = require('./util.js');
+const fs = require('fs-extra');
+const Nightmare = require('nightmare');
+const screenshotSelector = require('nightmare-screenshot-selector');
+const path = require('path');
+const PropertiesReader = require('properties-reader');
+const util = require('./util.js');
 
 Nightmare.action('screenshotSelector', screenshotSelector);
 
 async function main(argv) {
   const script = require(argv[2]);
   const nightmare = Nightmare(script.nightmare_props);
+  let loginInfo = {};
+
+  if (argv[3] !== undefined) {
+    const properties = PropertiesReader(argv[3]);
+    loginInfo = {
+      username: properties.get('user.login.username'),
+      password: properties.get('user.login.password')
+    }
+  }
+
   const screenshotNames = await script.run({
     nightmare: nightmare,
-    screenshot_dir: './screenshots-temp/'
+    screenshot_dir: './screenshots-temp/',
+    loginToAtlas: function() {
+      nightmare.goto('https://cloud.mongodb.com/user#/atlas/login')
+      nightmare.wait('input[name="username"]');
+
+      if (loginInfo.username && loginInfo.password) {
+        nightmare.type('input[name="username"]', loginInfo.username);
+        nightmare.type('input[name="password"]', loginInfo.password);
+      }
+      else {
+        throw new Error("No login information specified");
+      }
+      nightmare.click('.login-form-submit-button');
+    }
   });
 
   // Compare the images
