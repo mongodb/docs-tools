3c4fbad0b1331a4902e8a4c61a886442cfd8ed96
Andrew Aldridge <i80and@foxquill.com>
2018-06-01 13:20:54 -0400

    world-builder: Make more reliable, add missing versions

--- tools/world-builder/build.py
+++ tools/world-builder/build.py
@@ -2,22 +2,19 @@
 import fnmatch
 import json
 import os
-import pathlib
 import re
 import subprocess
 import sys
-from typing import List
+from typing import List, Optional
 
 PAT_URL = re.compile('.+/(.+).git')
 
 
-def git_get_commit_hash(branch: str, build_dir: str) -> str:
-    return str(subprocess.check_output(['git', 'rev-parse', 'master'], cwd=build_dir), 'utf-8').strip()
-
-
 class BuildTask:
-    def __init__(self, build_dir: str, build_command: str) -> None:
+    def __init__(self, branch: str, build_dir: str, build_redirects_command: Optional[str], build_command: str) -> None:
+        self.branch = branch
         self.build_dir = build_dir
+        self.build_redirects_command = build_redirects_command
         self.build_command = build_command
         self.built_flag_file = os.path.join(self.build_dir, '.built')
 
@@ -32,22 +29,31 @@ class BuildTask:
             except OSError:
                 pass
 
+        if self.build_redirects_command is not None:
+            checkout(self.build_dir, 'master')
+            subprocess.check_call(self.build_redirects_command, shell=True, cwd=self.build_dir)
+
+        checkout(self.build_dir, self.branch)
         subprocess.check_call(self.build_command, shell=True, cwd=self.build_dir)
 
 
-def clone(url: str, branch: str, dest: str) -> None:
+def clone(url: str, dest: str) -> None:
     subprocess.check_call(['git', 'clone', url, dest])
-    subprocess.check_call(['git', 'checkout', branch], cwd=dest)
 
 
-def pull(path: str, branch: str) -> None:
+def fetch(path: str) -> None:
     subprocess.check_call(['git', 'fetch', '-q'], cwd=path)
-    subprocess.check_call(['git', 'reset', '-q', '--hard'], cwd=path)
+
+
+def checkout(path: str, branch: str) -> None:
     subprocess.check_call(['git', 'checkout', '-q', branch], cwd=path)
+    subprocess.check_call(['git', 'reset', '--hard', 'origin/{}'.format(branch)], cwd=path)
 
 
 def build(project, patterns) -> List[BuildTask]:
-    project_name = PAT_URL.match(project['git']).group(1)
+    project_name = project.get('name', None)
+    if not project_name:
+        project_name = PAT_URL.match(project['git']).group(1)
 
     try:
         os.mkdir('build')
@@ -68,11 +74,11 @@ def build(project, patterns) -> List[BuildTask]:
 
         output_dir = os.path.join('build', full_branch_name)
         if not os.path.isdir(output_dir):
-            clone(project['git'], branch, output_dir)
+            clone(project['git'], output_dir)
         else:
-            pull(output_dir, branch)
+            fetch(output_dir)
 
-        if project.get('docs-tools', False):
+        if project.get('docs-tools', True):
             try:
                 os.mkdir(os.path.join(output_dir, 'build'))
             except FileExistsError:
@@ -83,7 +89,14 @@ def build(project, patterns) -> List[BuildTask]:
             except FileExistsError:
                 pass
 
-        build_tasks.append(BuildTask(output_dir, project['build']))
+        build_tasks.append(
+            BuildTask(
+                branch,
+                output_dir,
+                project.get(
+                    'build_redirects',
+                    "mut-redirects config/redirects -o build/public/.htaccess"),
+                project['build']))
 
     return build_tasks
 
@@ -96,19 +109,30 @@ def main(path: str, patterns_text: List[str]):
 
     # Pull docs-tools
     if not os.path.isdir('build/docs-tools'):
-        clone('https://github.com/mongodb/docs-tools.git', 'master', 'build/docs-tools')
+        clone('https://github.com/mongodb/docs-tools.git', 'build/docs-tools')
     else:
-        pull('build/docs-tools', 'master')
+        fetch('build/docs-tools')
+
+    checkout('build/docs-tools', 'master')
 
     tasks = []
     for project in data:
         tasks.extend(build(project, patterns))
 
+
+    errors = []  # type: List[str]
     for task in tasks:
         try:
             task.build()
         except subprocess.SubprocessError:
-            print('Failed to build!')
+            errors.append(task.build_dir)
+            print('Failed to build in {}'.format(task.build_dir))
+
+    if errors:
+        print('\nFailed to build:')
+        for d in errors:
+            print('  ' + d)
+
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
