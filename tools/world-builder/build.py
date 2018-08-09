aae8f58390fa6f9860377dcf2ad8fab9d5b230d4
Andrew Aldridge <i80and@foxquill.com>
2018-08-09 10:01:10 -0400

    world-builder: another step forward

--- tools/world-builder/build.py
+++ tools/world-builder/build.py
@@ -1,27 +1,25 @@
 #!/usr/bin/env python3
-import fnmatch
 import json
 import os
 import re
 import subprocess
 import sys
-from typing import List, Optional
+import yaml
+from typing import Any, Dict, Optional, Set, List
 
 PAT_URL = re.compile('.+/(.+).git')
 
 
 class BuildTask:
-    def __init__(self, branch: str, build_dir: str, build_redirects_command: Optional[str], build_command: str) -> None:
-        self.branch = branch
+    def __init__(self, name: str, branches: List[str], build_dir: str, build_command: str) -> None:
+        self.name = name
+        self.branches = branches
         self.build_dir = build_dir
-        self.build_redirects_command = build_redirects_command
         self.build_command = build_command
         self.built_flag_file = os.path.join(self.build_dir, '.built')
 
     def build(self) -> None:
-        print('Building ' + self.build_dir)
         subprocess.check_call(['git', 'clean', '-xfd'], cwd=self.build_dir)
-
         # Some of our properties die if these directories do not exist
         for path in ('build', 'build/master', 'build/public', 'build/public/master'):
             try:
@@ -29,96 +27,54 @@ class BuildTask:
             except OSError:
                 pass
 
-        if self.build_redirects_command is not None:
-            checkout(self.build_dir, 'master')
-            subprocess.check_call(self.build_redirects_command, shell=True, cwd=self.build_dir)
-
-        checkout(self.build_dir, self.branch)
-        subprocess.check_call(self.build_command, shell=True, cwd=self.build_dir)
-
-
-def clone(url: str, dest: str) -> None:
-    subprocess.check_call(['git', 'clone', url, dest])
+        for branch in self.branches:
+            print('Building ' + self.build_dir)
+            subprocess.check_call(['git', 'checkout', '-q', branch], cwd=self.build_dir)
+            subprocess.check_call(['git', 'pull', '--ff-only', '-q'], cwd=self.build_dir)
+            subprocess.check_call(self.build_command, shell=True, cwd=self.build_dir)
 
 
-def fetch(path: str) -> None:
-    subprocess.check_call(['git', 'fetch', '-q'], cwd=path)
+def load_branches_from_yaml(path: str) -> List[str]:
+    with open(path, 'r') as f:
+        loaded = yaml.safe_load(f)
+
+    return loaded['git']['branches']['published']
 
 
-def checkout(path: str, branch: str) -> None:
-    subprocess.check_call(['git', 'checkout', '-q', branch], cwd=path)
-    subprocess.check_call(['git', 'reset', '--hard', 'origin/{}'.format(branch)], cwd=path)
-
-
-def build(project, patterns) -> List[BuildTask]:
+def initialize_project(project: Dict[str, Any]) -> BuildTask:
+    git_url = project['git']
     project_name = project.get('name', None)
     if not project_name:
-        project_name = PAT_URL.match(project['git']).group(1)
+        match = PAT_URL.match(git_url)
+        if not match:
+            raise ValueError('Cannot determine project name: {}'.format(git_url))
+        project_name = match.group(1)
 
-    try:
-        os.mkdir('build')
-    except OSError:
-        pass
+    branches = project['branches']
+    if isinstance(branches, str):
+        branches = load_branches_from_yaml(branches)
 
-    build_tasks = []
-    for branch in project['branches']:
-        full_branch_name = '-'.join((project_name, branch))
-        matched = False
-        for pattern in patterns:
-            if pattern.match(full_branch_name):
-                matched = True
-                break
+    output_dir = os.path.join('build', project_name)
+    if not os.path.isdir(output_dir):
+        print('Cloning {}'.format(git_url))
+        subprocess.check_call(['git', 'clone', '-q', git_url, output_dir])
 
-        if not matched:
-            continue
-
-        output_dir = os.path.join('build', full_branch_name)
-        if not os.path.isdir(output_dir):
-            clone(project['git'], output_dir)
-        else:
-            fetch(output_dir)
-
-        if project.get('docs-tools', True):
-            try:
-                os.mkdir(os.path.join(output_dir, 'build'))
-            except FileExistsError:
-                pass
-
-            try:
-                os.symlink('../../docs-tools', os.path.join(output_dir, 'build', 'docs-tools'))
-            except FileExistsError:
-                pass
-
-        build_tasks.append(
-            BuildTask(
-                branch,
-                output_dir,
-                project.get(
-                    'build_redirects',
-                    "mut-redirects config/redirects -o build/public/.htaccess"),
-                project['build']))
-
-    return build_tasks
+    return BuildTask(
+            project_name,
+            branches,
+            output_dir,
+            project['build'])
 
 
-def main(path: str, patterns_text: List[str]):
+def main(path: str, projects: Optional[Set[str]]) -> None:
     with open(path, 'r') as f:
         data = json.load(f)
 
-    patterns = [re.compile(fnmatch.translate(text)) for text in patterns_text]
-
-    # Pull docs-tools
-    if not os.path.isdir('build/docs-tools'):
-        clone('https://github.com/mongodb/docs-tools.git', 'build/docs-tools')
-    else:
-        fetch('build/docs-tools')
-
-    checkout('build/docs-tools', 'master')
-
     tasks = []
     for project in data:
-        tasks.extend(build(project, patterns))
-
+        task = initialize_project(project)
+        if projects is None or task.name in projects:
+            tasks.append(task)
 
     errors = []  # type: List[str]
     for task in tasks:
@@ -136,8 +92,8 @@ def main(path: str, patterns_text: List[str]):
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        patterns = sys.argv[1:]
+        projects = set(sys.argv[1:])  # type: Optional[Set[str]]
     else:
-        patterns = ['*']
+        projects = None
 
-    main('projects.json', patterns)
+    main('projects.json', projects)
