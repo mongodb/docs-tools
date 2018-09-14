a05ee42dee0938f80ced509e81b522f7da6932eb
Andrew Aldridge <i80and@foxquill.com>
2018-09-14 14:09:28 -0400

    DOCSP-3292: icon: Only approved icons allowed

--- sphinxext/icon.py
+++ sphinxext/icon.py
@@ -1,11 +1,31 @@
+import re
 from docutils import nodes
 from docutils.parsers.rst import roles
 from sphinx.util.nodes import split_explicit_title
 
+PAT_STRIP = re.compile(r'-(?:o|(?:alt))$')
+
+# A pre-approved list of icons. Approval required to add to this list.
+# Make sure you update the documentation in github.com:mongodb/docs-meta
+# as well when updating.
+ICONS = set([
+    'caret-right',
+    'caret-square-left',
+    'caret-square-right',
+    'check-circle',
+    'check-square',
+    'edit',
+    'exclamation-circle',
+    'pencil',
+    'save',
+    'trash',
+    'trash-alt'
+])
+
 
 def class_to_label(css_class):
     """Approximate a label for screen readers from a FontAwesome class."""
-    return css_class.rstrip('-o').replace('-', ' ') + ' icon'
+    return PAT_STRIP.sub('', css_class).replace('-', ' ') + ' icon'
 
 
 class IconNode(nodes.General, nodes.Inline, nodes.Element):
@@ -16,7 +36,7 @@ class IconNode(nodes.General, nodes.Inline, nodes.Element):
             self.starttag(
                 node,
                 'span',
-                CLASS='fa fa-{}'.format(node['css_class']),
+                CLASS='fas fa-{}'.format(node['css_class']),
                 **{'title': node['label']}))
         self.body.append(self.starttag(node, 'span', CLASS='screenreader'))
         self.body.append(node['label'])
@@ -27,14 +47,18 @@ class IconNode(nodes.General, nodes.Inline, nodes.Element):
 
 
 def icon_role(typ, rawtext, text, lineno, inliner, options={}, content=[]):
+    errors = []
     has_explicit_title, label, css_class = split_explicit_title(text)
+    if css_class not in ICONS:
+        errors.append(inliner.reporter.error('Unknown icon: ' + css_class, line=lineno))
+
     label = label if has_explicit_title else class_to_label(css_class)
 
     config = inliner.document.settings.env.config
     if config._raw_config['tags'].eval_condition('html'):
         return [IconNode(css_class=css_class, label=label)], []
 
-    return [nodes.Text(label)], []
+    return [nodes.Text(label)], errors
 
 
 def setup(app):
