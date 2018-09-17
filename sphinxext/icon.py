4c25611558c2bd034ba942f956f33b60f640bbf3
kay <kay.kim@10gen.com>
2018-09-17 11:36:16 -0400

    DOCSP-3274: use font awesome 5

--- sphinxext/icon.py
+++ sphinxext/icon.py
@@ -22,6 +22,9 @@ ICONS = set([
     'trash-alt'
 ])
 
+ICONSB = set([
+    'windows',
+])
 
 def class_to_label(css_class):
     """Approximate a label for screen readers from a FontAwesome class."""
@@ -36,7 +39,7 @@ class IconNode(nodes.General, nodes.Inline, nodes.Element):
             self.starttag(
                 node,
                 'span',
-                CLASS='fa fa-{}'.format(node['css_class']),
+                CLASS='fas fa-{}'.format(node['css_class']),
                 **{'title': node['label']}))
         self.body.append(self.starttag(node, 'span', CLASS='screenreader'))
         self.body.append(node['label'])
@@ -60,10 +63,43 @@ def icon_role(typ, rawtext, text, lineno, inliner, options={}, content=[]):
 
     return [nodes.Text(label)], errors
 
+class IconBrandNode(nodes.General, nodes.Inline, nodes.Element):
+    @staticmethod
+    def visit_icon(self, node):
+        # I've tested this in Safari w/ macOS Voiceover
+        self.body.append(
+            self.starttag(
+                node,
+                'span',
+                CLASS='fab fa-{}'.format(node['css_class']),
+                **{'title': node['label']}))
+        self.body.append(self.starttag(node, 'span', CLASS='screenreader'))
+        self.body.append(node['label'])
+
+    @staticmethod
+    def depart_icon(self, node=None):
+        self.body.append('</span></span>\n')
+
+
+def icon_brand_role(typ, rawtext, text, lineno, inliner, options={}, content=[]):
+    errors = []
+    has_explicit_title, label, css_class = split_explicit_title(text)
+    if css_class not in ICONSB:
+        errors.append(inliner.reporter.error('Unknown icon: ' + css_class, line=lineno))
+
+    label = label if has_explicit_title else class_to_label(css_class)
+
+    config = inliner.document.settings.env.config
+    if config._raw_config['tags'].eval_condition('html'):
+        return [IconBrandNode(css_class=css_class, label=label)], []
+
+    return [nodes.Text(label)], errors
 
 def setup(app):
     app.add_node(IconNode, html=(IconNode.visit_icon, IconNode.depart_icon))
     roles.register_local_role('icon', icon_role)
+    app.add_node(IconBrandNode, html=(IconBrandNode.visit_icon, IconBrandNode.depart_icon))
+    roles.register_local_role('iconb', icon_brand_role)
 
     return {
         'parallel_read_safe': True,
