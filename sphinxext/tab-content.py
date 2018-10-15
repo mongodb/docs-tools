ecd66eff3df83f5a4e478c58a1925f2f0d63f6f4
Nick Larew <nick.larew@10gen.com>
2018-10-15 17:59:55 -0400

    Add tab-bar directive

--- sphinxext/tab-content.py
+++ sphinxext/tab-content.py
@@ -8,13 +8,49 @@ import yaml
 
 TAB_CONTENT_TEMPLATE = fett.Template('''
 .. {{ tabset }}::
-   
+
    hidden: true
    tabs:
      - id: {{ tab_id }}
        content: |
          {{ content }}
 ''')
+
+TAB_BAR_TEMPLATE = fett.Template('''
+.. {{ tabset }}::
+
+   tabs:
+   {{ for tab in tabs }}
+     - id: {{ tab }}
+       content: ""
+
+   {{ end }}
+''')
+
+
+class TabBar(Directive):
+    has_content = False
+    required_arguments = 1
+    optional_arguments = 0
+    final_argument_whitespace = False
+    option_spec = {
+        'tab-ids': directives.unchanged_required
+    }
+
+    def run(self):
+        tabset = self.arguments[0]
+        tabs = [id.strip() for id in self.options['tab-ids'].split(",")]
+
+        rendered = TAB_BAR_TEMPLATE.render({
+            "tabset": tabset,
+            "tabs": tabs,
+        })
+        rendered_lines = statemachine.string2lines(
+            rendered, 4, convert_whitespace=1
+        )
+        self.state_machine.insert_input(rendered_lines, '')
+
+        return []
 
 
 class TabContent(Directive):
@@ -25,7 +61,7 @@ class TabContent(Directive):
     option_spec = {
         'tab-id': directives.unchanged_required
     }
-    
+
     def run(self):
         options = self.options
         tabset = self.arguments[0]
@@ -40,11 +76,12 @@ class TabContent(Directive):
             rendered, 4, convert_whitespace=1
         )
         self.state_machine.insert_input(rendered_lines, '')
-        
+
         return []
 
 
 def setup(app):
+    app.add_directive('tab-bar', TabBar)
     app.add_directive('tab-content', TabContent)
 
     return {'parallel_read_safe': True,
