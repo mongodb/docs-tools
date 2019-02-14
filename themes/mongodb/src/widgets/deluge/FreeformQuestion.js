58a1d261210185e08b65d1b71ac3e60b7377d124
sophstad <srs2231@columbia.edu>
2019-02-14 13:55:28 -0500

    DOCSP-4484: Feedback form min char count

--- themes/mongodb/src/widgets/deluge/FreeformQuestion.js
+++ themes/mongodb/src/widgets/deluge/FreeformQuestion.js
@@ -1,16 +1,64 @@
 import PropTypes from 'prop-types';
 import preact from 'preact';
 
-export default function FreeformQuestion({placeholder, store}) {
-    return (
-        <textarea
-            placeholder={placeholder}
-            onChange={(ev) => store.set(ev.target.value)}
-            value={store.get() || ''}></textarea>
-    );
+const MIN_CHAR_COUNT = 15;
+const ERROR_TEXT = 'Please provide at least 15 characters in your response.';
+
+class FreeformQuestion extends preact.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            'error': false,
+            'text': ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(ev) {
+        const {store} = this.props;
+        const value = ev.target.value;
+
+        if (value === '' || value.length >= MIN_CHAR_COUNT) {
+            this.setState({
+                'error': false,
+                'text': value
+            });
+            store.set(value);
+            ev.target.setCustomValidity('');
+        } else {
+            this.setState({
+                'error': true,
+                'text': value
+            });
+            store.set('');
+            ev.target.setCustomValidity(ERROR_TEXT);
+        }
+    }
+
+    render() {
+        const {placeholder} = this.props;
+        const {error, text} = this.state;
+        return (
+            <div>
+                <textarea
+                    placeholder={placeholder}
+                    onInput={this.handleChange}
+                    value={text}></textarea>
+                <div
+                    className="error"
+                    style={{'visibility': error ? 'visible' : 'hidden'}}>
+                    {ERROR_TEXT}
+                </div>
+            </div>
+        );
+    }
 }
 
 FreeformQuestion.propTypes = {
     'placeholder': PropTypes.string,
     'store': PropTypes.objectOf(PropTypes.func).isRequired
 };
+
+export default FreeformQuestion;
