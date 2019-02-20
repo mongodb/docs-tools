58d4af960b272e11752b0845ff3c3758b7dbdb0b
Sophie Stadler <sophstad@users.noreply.github.com>
2019-02-20 10:47:19 -0500

    DOCSP-4484: Feedback form min char count (#263)
    * DOCSP-4484: Feedback form min char count
    
    * Update error message
    
    * Refactor, disable submit button when text too short

--- themes/mongodb/src/widgets/deluge/FreeformQuestion.js
+++ themes/mongodb/src/widgets/deluge/FreeformQuestion.js
@@ -1,16 +1,64 @@
 import PropTypes from 'prop-types';
 import preact from 'preact';
 
-export default function FreeformQuestion({placeholder, store}) {
-    return (
-        <textarea
-            placeholder={placeholder}
-            onChange={(ev) => store.set(ev.target.value)}
-            value={store.get() || ''}></textarea>
-    );
+class FreeformQuestion extends preact.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            'error': false,
+            'text': ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(ev) {
+        const {hasError, store} = this.props;
+        const value = ev.target.value;
+        const error = hasError(value);
+
+        if (error) {
+            store.set('');
+            ev.target.setCustomValidity(error);
+        } else {
+            store.set(value);
+            ev.target.setCustomValidity('');
+        }
+        this.setState({
+            'error': error,
+            'text': value
+        });
+    }
+
+    render() {
+        const {errorText, placeholder} = this.props;
+        const {error, text} = this.state;
+        return (
+            <div>
+                <textarea
+                    placeholder={placeholder}
+                    onInput={this.handleChange}
+                    value={text}></textarea>
+                <div
+                    className="error"
+                    style={{'visibility': error ? 'visible' : 'hidden'}}>
+                    {errorText}
+                </div>
+            </div>
+        );
+    }
 }
 
 FreeformQuestion.propTypes = {
+    'errorText': PropTypes.string,
+    'hasError': PropTypes.func,
     'placeholder': PropTypes.string,
     'store': PropTypes.objectOf(PropTypes.func).isRequired
 };
+
+FreeformQuestion.defaultProps = {
+    'hasError': () => false
+};
+
+export default FreeformQuestion;
