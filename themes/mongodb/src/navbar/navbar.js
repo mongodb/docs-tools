a36d79483b0b4cf716ae7b6bb445ea19fe347e7f
Grace Chong <grace.chong@mongodb.com>
2022-05-26 16:20:32 -0400

    fix src

--- themes/mongodb/src/navbar/navbar.js
+++ themes/mongodb/src/navbar/navbar.js
@@ -187,7 +187,7 @@ class Navbar extends preact.Component {
             <nav className="navbar">
                 <div className="navbar__left">
                     <a href="https://www.mongodb.com/">
-                        <img src={'snooty-koueq.mongodbstitch.com/logo-mongodb.png'} className="navbar-brand" alt="MongoDB Logo" />
+                        <img src="https://snooty-koueq.mongodbstitch.com/logo-mongodb.png" className="navbar-brand" alt="MongoDB Logo" />
                     </a>
 
                     <span className="navbar-seperator"></span>
